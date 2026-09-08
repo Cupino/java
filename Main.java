2234e6e0531ca72141dd5946f566d505c70d4012
Cupino <cupeckubko@gmail.com>
2026-09-08 19:58:50 +0200

    kalkulacka hotova cela aj krat deleno

--- Main.java
+++ Main.java
@@ -1,40 +1,42 @@
 package com.company;
 
-        import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        //float i = 3.4f;
-        //float y = 4.9f;
-        //System.out.println(i + y);
 
-        //int a = 4;
-        //int b = 5;
-
-        //if (a == b) {
-        //    System.out.println("A sa rovna b");
-        //} else {
-        //    System.out.println("A sa nerovna b");
-        //}
         int vysledok;
+
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Zadaj cislo a:");
         int a = scanner.nextInt();
+
         System.out.println("Zadaj cislo b:");
         int b = scanner.nextInt();
-        System.out.println("Chces cisla scitat (1) alebo odcitat (2)?");
-        int operacia = scanner.nextInt();
 
-        if (operacia == 1){
+        System.out.println("Chces cisla scitat (+), odcitat (-), nasobit (*) alebo delit (/)?");
+        String operacia = scanner.next();
+
+        if (operacia.equals("+")) {
             vysledok = a + b;
             System.out.println("Vysledok je: " + vysledok);
         }
-        else {
+        else if (operacia.equals("-")) {
             vysledok = a - b;
-            System.out.println("Vysledok je:" + vysledok);
+            System.out.println("Vysledok je: " + vysledok);
         }
-
-
+        else if (operacia.equals("*")) {
+            vysledok = a * b;
+            System.out.println("Vysledok je: " + vysledok);
+        }
+        else if (operacia.equals("/")) {
+            vysledok = a / b;
+            System.out.println("Vysledok je: " + vysledok);
+        }
+        else {
+            System.out.println("Neplatna operacia!");
+        }
     }
 }
